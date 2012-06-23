c609bfa20aa5a8c3aea53e5b3771930af627d608
Louis Vera <imnotatwork@gmail.com>
2012-06-23 18:23:41 -0400

    removing unnecessary comment

--- Tests/model/UKModelTests.h
+++ Tests/model/UKModelTests.h
@@ -13,9 +13,6 @@
 //   See the License for the specific language governing permissions and
 //   limitations under the License.
 
-//  Application unit tests contain unit test code that must be injected into an application to run correctly.
-//  Define USE_APPLICATION_UNIT_TEST to 0 if the unit test code is designed to be linked into an independent test executable.
-
 #import <SenTestingKit/SenTestingKit.h>
 #import "UKModel.h"
 
